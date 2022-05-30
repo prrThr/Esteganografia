c86b7ac9eedffa77e783e4c93f73c69a29d3d3fc
prrThr <arthur.op9@gmail.com>
2022-05-30 15:44:50 -0300

    Trabalho pronto!

--- codigo.py
+++ codigo.py
@@ -1,24 +1,9 @@
 import numpy as np
 import cv2 as cv
 
-img = cv.imread('parrot.jpg')
-secret = cv.imread('secret.jpg')
-
-#print(f"\nDimensões da imagem: {img.shape}")                #* Tirar comentário após terminar
-
-altura, largura, canais = img.shape
-lastPixel = img[altura-1, largura-1, 2]
-
-#print(f"Último pixel em:  X:{altura}   Y:{largura}")        #* Tirar comentário após terminar
-#print(f"Nível de vermelho no ultimo pixel = {lastPixel}")   #* Tirar comentário após terminar
 
 def bitfield(n):
     return [int(digit) for digit in bin(n)[2:]]
-
-#lastPixel_binario = bitfield(lastPixel) #! Provavelmente nao vai usar o lastPixel
-#print(f"{lastPixel} em binario = {lastPixel_binario}")     #* Tirar comentário após terminar
-
-
 
 
 def gerar_mensagem(mensagem):
@@ -36,14 +21,8 @@ def gerar_mensagem(mensagem):
     return arr
 
 
-texto = input(str("Digite sua mensagem secreta: "))
-arrayBits = gerar_mensagem(texto)  # Transformar o TEXTO em BINÁRIO
-# print(arrayBits)                                       #* Tirar comentário após terminar
-
-img2 = img.copy()
-
-
-def encriptar(img2):
+def encriptar(img2, arrayBits, altura, largura):
+    input("Presione ENTER para encriptar dentro da imagem")
     i = 0
     pararLoop = False
 
@@ -52,13 +31,8 @@ def encriptar(img2):
             break
         for x in range(largura-1, 0, -1):
             if i == arrayBits.size-1:
-                print(f"Parou no indice {i}")
-                print(f"Valor binario final: {arrayBits[i-1]} Y:{y} x:{x+1}")
                 pararLoop = True
                 break
-
-            if i == 0:
-                print(f"Valor binario inicial: {arrayBits[i]} Y:{y} x:{x}")
 
             if arrayBits[i] == 0 and img2[y, x, 2] % 2 != 0:
                 img2[y, x, 2] = img2[y, x, 2] - 1
@@ -66,34 +40,11 @@ def encriptar(img2):
             if arrayBits[i] == 1 and img2[y, x, 2] % 2 == 0:
                 img2[y, x, 2] = img2[y, x, 2] - 1
             i = i + 1
+    print("Mensagem encriptada!")
 
-
-encriptar(img2)
-
-'''
-def debuggin(altura, largura, arrayBits):
-    i = 0
-    pararLoop = False
-    for y in range(altura-1, 0, -1):
-        if pararLoop == True:
-            break
-        for x in range(largura-1, 0, -1):
-            if i == 25:
-                print(f"Parou no indice {i}")
-                print(f"Valor binario final: {arrayBits[i-1]} Y:{y} x:{x}")
-                pararLoop = True
-                break
-                
-            print(f"\nValor binario atual = {arrayBits[i]}")
-            print(f"img  -> Y:{y} X:{x} - Red Value = {img[y, x, 2]}")
-            print(f"img2 -> Y:{y} X:{x} - Red Value = {img2[y, x, 2]}")
-            i = i + 1
-
-
-debuggin(altura, largura, arrayBits)
-'''
 
 def converter_mensagem(saida):
+    input("Presione ENTER para decriptar a mensagem")
     bits = np.array(saida)
     mensagem_out = ''
     bits = bits.reshape((int(len(saida)/8), 8))
@@ -105,7 +56,8 @@ def converter_mensagem(saida):
     return mensagem_out
 
 
-def lastRed_secretos(img2):
+def lastRed_secretos(img2, arrayBits, altura, largura):
+    input("Presione ENTER para procurar os ultimos valores de vermelho dentro da imagem")
     lista = []
     i = 0
     pararLoop = False
@@ -132,7 +84,9 @@ def lastRed_secretos(img2):
             i = i + 1
         return lista
 
+
 def lastBits_secretos(lastReds):
+    input("Presione ENTER para retornar a mensagem em binario")
     lista = []
     for i in range(len(lastReds)):
         aux = bitfield(lastReds[i])
@@ -140,15 +94,34 @@ def lastBits_secretos(lastReds):
     arr = np.array(lista)
     arr = arr.flatten()
     return arr
-    
-
-lastReds = []    
-lastReds = lastRed_secretos(img2)
-lastBits = lastBits_secretos(lastReds)
-print(f"ArrayBits = {arrayBits}")
-#print(f"Last Reds = {lastReds}")
-print(f"Last Bits = {lastBits}")
-print(f"ArrayBits traduzido = {converter_mensagem(arrayBits)}") 
-print(f"Bits Secretos traduzido = {converter_mensagem(lastBits)}") 
 
 
+def main():
+    img = cv.imread('parrot.jpg')
+    secret = cv.imread('secret.jpg')
+
+    texto = input(str("Digite sua mensagem secreta: "))
+    arrayBits = gerar_mensagem(texto)  # Transformar o TEXTO em BINÁRIO
+
+    print(f"\n\nDimensões da imagem: {img.shape}")
+
+    altura, largura, canais = img.shape
+    lastPixel = img[altura-1, largura-1, 2]
+
+    img2 = img.copy()
+    encriptar(img2, arrayBits, altura, largura)
+
+    lastReds = []
+    lastReds = lastRed_secretos(img2, arrayBits, altura, largura)
+    print(f"Ultimos valores vermelhos = {lastReds}")
+    lastBits = lastBits_secretos(lastReds)
+    print(f"Mensagem nao traduzida = {lastBits}")
+    # print(f"ArrayBits = {arrayBits}")   Apenas para
+    # print(f"Last Reds = {lastReds}")    verificar se
+    # print(f"Last Bits = {lastBits}")    estão certos
+    #print(f"ArrayBits traduzido = {converter_mensagem(arrayBits)}")
+    print(f"\nMensagem traduzida = {converter_mensagem(lastBits)}")
+    print("Mensagem decriptada com sucesso!")
+
+
+main()
